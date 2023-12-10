37551f003fc31372b7592d4aed3410a216f0497b
robinDurstCS <robin.durst@contentsquare.com>
2023-12-10 23:41:29 +0100

    feat: estimate price with thresholds from json

--- index.ts
+++ index.ts
@@ -1,1 +1,4 @@
-console.log("Hello via Bun!");
+import fs from 'fs';
+
+const { ø0, ø1 } = JSON.parse(fs.readFileSync(import.meta.dir + '/thresholds.json', 'utf8'));
+export const estimatePrice = (mileage: number) => ø0 + (ø1 * mileage);
